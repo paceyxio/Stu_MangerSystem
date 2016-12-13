eaaccf5832e3a271d16ccb618de6a733f1cb672d
xiong <1833097998@qq.com>
2016-12-13 22:30:53 +0800

    输入法弹出页面上移

--- sqlitedemo/src/main/java/com/example/pacey/sqlitedemo/MainActivity.java
+++ sqlitedemo/src/main/java/com/example/pacey/sqlitedemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         setContentView(R.layout.activity_main);
         db = openOrCreateDatabase("user.db", MODE_PRIVATE, null);
         db.execSQL("create table if not exists usertb (_id integer primary key autoincrement,name text not null,address text,telephone text)");
